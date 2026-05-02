9747b19be3a0fc52e192cb14d93998d6f9d41b33
shub39 <cptnshubham39@gmail.com>
2026-05-02 09:49:40 +0530

    :arrow_up: bumped version
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ plugins {
 }
 
 val appName = "Grit"
-val appVersionCode = 5910
-val appVersionName = "5.9.1"
+val appVersionCode = 5920
+val appVersionName = "5.9.2"
 
 val gitHash = execute("git", "rev-parse", "HEAD").take(7)
 
